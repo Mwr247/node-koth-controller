1b1206dacdfe91bfadd72fe4b87094847346188b
Michael Rogers <mwr247@gmail.com>
2016-02-25 01:40:36 -0500

    Adding shooter game

---
+++ games/shooter/shooter.js
@@ -0,0 +1,54 @@
+/* globals cfg, util, bots */
+
+/*****
+Private
+*****/
+const local = {};
+
+// What data to give to each bot
+local.data = function(id) {
+	return id + ',' + local.rounds + ',' + bots[id].args;
+};
+
+// Handles bot responses
+local.response = function(result) {
+	util.out.print(result.output.trim(), 6);
+};
+
+/*****
+Public
+*****/
+const self = {};
+
+// Initial setup
+self.init = function(data) {
+  local.rounds = data[0] || cfg.game.rules.rounds;
+
+	for (var bot in bots) {
+		bot.state = {
+			alive: true,
+			loaded: false,
+			blocked: false
+		};
+	}
+
+  util.out.log('Starting game (' + local.rounds + ' rounds)...', 3);
+
+  self.time = Date.now();
+  self.run();
+};
+
+// Run a round
+self.run = function() {
+	if (local.rounds > 0) {
+    local.rounds--;
+    util.run.async(bots, local.data, local.response);
+  } else {
+    util.out.log('Run complete: ' + (Date.now() - self.time) / 1000 + ' seconds.', 3);
+  }
+};
+
+/*****
+Export
+*****/
+module.exports = self;
