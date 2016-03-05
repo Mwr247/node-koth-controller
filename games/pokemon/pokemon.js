a5440a8b8f76a4d0b3b490adcac1e7ededd1dd16
Michael Rogers <mwr247@gmail.com>
2016-03-04 22:16:51 -0500

    Pokemon starter code

---
+++ games/pokemon/pokemon.js
@@ -0,0 +1,61 @@
+/* globals cfg, util, bots */
+const fs = require('fs');
+const pokedex = JSON.parse(fs.readFileSync('./pokedex.json', 'utf8'));
+const local = {};
+
+// Initial setup
+local.init = function(data) {
+  local.rounds = cfg.game.rules.rounds = data[0] || cfg.game.rules.rounds;
+
+  local.pairings = local.shuffle(bots.length);
+
+  util.out.log(local.pairings, pokedex.damageEquation);
+
+  util.out.log('Starting run (' + local.rounds + ' rounds)...', 3);
+
+  local.time = Date.now();
+  local.run();
+};
+
+// Shuffle a list, or create a list of shuffled numbers
+local.shuffle = function(list) {
+  if (list.length == null && list >= 0) {
+    list = Array(list).fill(0).map(function(val, i) {
+      return i;
+    });
+  }
+  for (var i = list.length - 1; i > 0; i--) {
+    var j = Math.random() * (i + 1) | 0;
+    [list[i], list[j]] = [list[j], list[i]];
+  }
+  return list;
+};
+
+// Run a round
+local.run = function() {
+    util.run.async(bots, local.botData, local.botResponse, local.postRun);
+};
+
+// Handle post-round logic
+local.postRun = function() {
+	local.rounds--;
+	if (local.rounds > 0) {
+		local.run();
+	} else {
+		util.out.log('Run complete: ' + (Date.now() - local.time) / 1000 + ' seconds.', 3);
+	}
+};
+
+// What data to give to each bot
+local.botData = function(id) {
+	return id + ',' + local.rounds + ',' + bots[id].args;
+};
+
+// Handles bot responses
+local.botResponse = function(result) {
+	util.out.print(result.data.trim(), 6);
+};
+
+module.exports = {
+	init: local.init
+};
